1cde64b0018b52930ce26280e3a219a5c5109114
Greg Lightfoot <glightfoot@digitalocean.com>
2021-04-28 15:36:32 -0400

    go mod init/tidy/vendor

--- client.go
+++ client.go
@@ -1,7 +1,7 @@
 package gosocketio
 
 import (
-	"github.com/graarh/golang-socketio/transport"
+	"github.com/glightfoot/golang-socketio/transport"
 	"strconv"
 )
 
--- examples/server.go
+++ examples/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/graarh/golang-socketio"
-	"github.com/graarh/golang-socketio/transport"
+	"github.com/glightfoot/golang-socketio"
+	"github.com/glightfoot/golang-socketio/transport"
 	"log"
 	"net/http"
 	"time"
